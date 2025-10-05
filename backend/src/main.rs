da8a74185adba851f92708e6ce5073f11d24d633
Josiah Glosson <soujournme@gmail.com>
2025-10-05 12:20:46 -0500

    Set up logging

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,16 +1,49 @@
+use std::error::Error;
+use std::ffi::OsStr;
+use std::process;
+use std::str::FromStr;
+use tracing_subscriber::EnvFilter;
+use tracing_subscriber::filter::LevelFilter;
+
 mod book_data;
 mod reference;
 
+#[derive(Debug, thiserror::Error)]
+enum ServerError {
+    #[error("Environment error for variable {0}: {1}")]
+    Env(String, #[source] dotenvy::Error),
+    #[error("Invalid value '{1}' for environment variable {0}: {2}")]
+    EnvParse(String, String, #[source] Box<dyn Error + Send + 'static>),
+    #[error("Invalid logging configuration: {0}")]
+    TracingEnv(#[from] tracing_subscriber::filter::FromEnvError),
+    #[error("IO error: {0}")]
+    Io(#[from] std::io::Error)
+}
+
 fn main() {
-    let full_ref = "James 1:1-4;Hosea4;Lk6:1-14;7,9:1-9,10:16";
-    // let full_ref = "Beginning";
-    let references = reference::parse_references(full_ref);
-    // println!("{references:#?}");
-    for reference in references {
-        let reference = reference.expect("Invalid reference");
-        println!(
-            "{:?} {}:{}-{}",
-            reference.book, reference.chapter, reference.verses.0, reference.verses.1
-        );
+    if let Err(e) = real_main() {
+        let _ = tracing_subscriber::fmt().try_init();
+        tracing::error!("Failed to run server: {e}");
+        process::exit(1);
     }
+}
+
+fn real_main() -> Result<(), ServerError> {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::builder().with_default_directive(LevelFilter::INFO.into()).from_env()?)
+        .init();
+    let bind_host: String = var("BIND_HOST")?;
+    let bind_port: String = var("BIND_PORT")?;
+    Ok(())
+}
+
+fn var<T: FromStr>(var_name: impl AsRef<OsStr>) -> Result<T, ServerError>
+where
+    T::Err: Error + Send + 'static
+{
+    let base_value = dotenvy::var(&var_name)
+        .map_err(|x| ServerError::Env(var_name.as_ref().display().to_string(), x))?;
+    let parsed_value = base_value.parse()
+        .map_err(|x| ServerError::EnvParse(var_name.as_ref().display().to_string(), base_value, Box::new(x)))?;
+    Ok(parsed_value)
 }
